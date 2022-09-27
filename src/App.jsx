b23e03039689e907432e69191610f28dbee9a176
MohamedBe <85387389+MohamedBelhedi@users.noreply.github.com>
2022-09-27 22:48:11 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,103 @@
+import './App.css'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import ReactAudioPlayer from 'react-audio-player'
+import papercss from 'papercss'
+export default function App() {
+  const [search, setSearch] = useState("")
+  const [result, setResult] = useState([])
+  const [music, setMusic] = useState([])
+  const [vis, setVis] = useState(true)
+  const [imgUri, settImgUri] = useState(["https://images.pexels.com/photos/1606655/pexels-photo-1606655.jpeg?cs=srgb&dl=pexels-suvan-chowdhury-1606655.jpg&fm=jpg"])
+  const [errBoud, setErrboun] = useState("")
+  const fetchData = (e) => {
+
+    const options = {
+      method: 'GET',
+      url: 'https://shazam.p.rapidapi.com/search',
+      params: { term: search, locale: 'en-US', offset: '0', limit: '5' },
+      headers: {
+        'X-RapidAPI-Key': '89b1d9a968msh60bd0851d8b138ep13474djsn004188407865',
+        'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
+      }
+    };
+    if (e.key === "Enter") {
+      axios.request(options).then(function(response) {
+        console.log(response.data.tracks.hits[0]);
+        console.log(response.data.tracks.hits[0].track.hub.actions[1]);
+        console.log(response.data.tracks.hits[0].track.images.coverart)
+        const randInt = Math.floor(Math.random() * 4)
+
+        setResult(response.data.tracks.hits[0].track === undefined ? alert("geht nicht") : response.data.tracks.hits[0].track)
+        setMusic(response.data.tracks.hits[0].track.hub.actions[1])
+        settImgUri(response.data.tracks.hits[0].track.images.coverart)
+
+
+        // error Boud weiter bearbeiten
+        { response.data.tracks.hits[0].track.hub.actions[1] === undefined ? alert("geht nicht") : null }
+
+      }).catch(function(error, TypeError) {
+        console.error(error);
+        setVis(false)
+        setResult("")
+        setErrboun(TypeError + "Fehler beim abspielen der Datei")
+        setTimeout(() => {
+          setVis(true)
+          setErrboun("")
+          setSearch("")
+        }, 3000)
+
+      });
+
+    }
+  }
+
+  useEffect(() => {
+
+
+
+  }, [])
+  return (
+    <>
+      <div className="d-flex justify-content-center align-self-center col">
+
+        <input onChange={(e) => {
+          console.log(e.target.value)
+          setSearch(e.target.value)
+        }} value={search} placeholder="search words from the lyrics...." onKeyPress={fetchData} />
+        <h1>{result.title === 0 ? <h1>not found</h1> : result.title}</h1>
+        <h1>{result.subtitle}</h1>
+        {vis ? <>
+          <div className="flex-md-column" >
+
+            {imgUri === [] ? settImgUri("https://images.pexels.com/photos/1606655/pexels-photo-1606655.jpeg?cs=srgb&dl=pexels-suvan-chowdhury-1606655.jpg&fm=jpg") : <img style={{ width: 300, height: 300 }} src={imgUri} alt={imgUri} />}
+
+            <ReactAudioPlayer
+              src={music.uri === undefined ? null : music.uri}
+              autoplay
+              controls
+            />
+          </div>
+        </>
+          : <>
+            <h3>{errBoud}</h3>
+            <h1>No Song found</h1>
+
+
+          </>
+
+
+
+        }
+
+      </div>
+
+
+      <div>
+
+
+
+      </div>
+    </>
+  )
+}
